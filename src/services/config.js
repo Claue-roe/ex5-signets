45340b0cf57867543b48de67ff4398d7d6cd811c
Claudia Patry <71995934+Claue-roe@users.noreply.github.com>
2021-03-31 18:05:39 -0400

    Finalisation vrai

--- src/services/config.js
+++ src/services/config.js
@@ -5,14 +5,7 @@
 
 // Configuration Firebase (mettez-y les vôtres !)
 const firebaseConfig = {
-  apiKey: "AIzaSyCALR26mfBqim8lJw9DongwsXO3agomG7Y",
-  authDomain: "panier-achats-cpatry.firebaseapp.com",
-  databaseURL: "https://panier-achats-cpatry-default-rtdb.firebaseio.com",
-  projectId: "panier-achats-cpatry",
-  storageBucket: "panier-achats-cpatry.appspot.com",
-  messagingSenderId: "834323949561",
-  appId: "1:834323949561:web:c4a89652abde78b46dff28",
-  measurementId: "G-N0T5DY59QR"
+  //secret hehehehe
 };
 export default firebaseConfig;
 
